2b6079146a88f7419a836bae5e9f1f6b3892ca26
akhilyed <158219372+akhilyed@users.noreply.github.com>
2026-06-05 11:01:03 -0400

    setup for website
    will start hosting with vercel

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to open the image in a new window/tab
+function openScreenshot(imgSrc) {
+    // Opens a new window with the image's source link
+    window.open(imgSrc, '_blank', 'noopener,noreferrer');
+}
